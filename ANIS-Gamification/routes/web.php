a8ea40353e7f5da5a206489ad6c3a79eea9a38bd
ajartijaouad <ajartijaouad14@gmail.com>
2026-04-14 16:16:46 +0100

    feat(routes): add register routes

--- ANIS-Gamification/routes/web.php
+++ ANIS-Gamification/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+Route::post('/register', [AuthController::class, 'register']);

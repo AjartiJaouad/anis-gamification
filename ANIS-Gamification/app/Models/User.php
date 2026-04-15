27719cfdd8b9b6d114d10f308ea069145733313e
ajartijaouad <ajartijaouad14@gmail.com>
2026-04-15 11:58:45 +0100

    fix(model): replace 'name' with 'pseudo' in fillable and add xp_total, streak_days - was causing pseudo to be silently dropped on User::create() leading to SQL error 1364

--- ANIS-Gamification/app/Models/User.php
+++ ANIS-Gamification/app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password', 'is_anonymous'])]
+#[Fillable(['pseudo', 'email', 'password', 'is_anonymous', 'xp_total', 'streak_days'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {

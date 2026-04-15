b4a977cb5bfea32cbf10f19ae917b894fa3a62e1
ajartijaouad <ajartijaouad14@gmail.com>
2026-04-15 12:19:08 +0100

    feat: create login blade view with Anis Ethos branding

--- ANIS-Gamification/resources/views/login.blade.php
+++ ANIS-Gamification/resources/views/login.blade.php
@@ -1,6 +1,15 @@
 <form action="{{ route('login') }}" method="POST" class="space-y-6">
     @csrf
-    <input name="pseudo" type="text" ... />
+
+    @if($errors->any())
+        <div class="text-error text-xs mb-4 p-2 bg-error/10 rounded">
+            {{ $errors->first() }}
+        </div>
+    @endif
+
+    <input name="pseudo" type="text" value="{{ old('pseudo') }}" ... />
+
     <input name="password" type="password" ... />
+
     <button type="submit">Login</button>
 </form>

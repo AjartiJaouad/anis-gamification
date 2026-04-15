37b239c3cba8bd3a425cb5c2b9acf9c0a61a1307
ajartijaouad <ajartijaouad14@gmail.com>
2026-04-15 12:21:22 +0100

    feat: define auth routes and protect dashboard with middleware

--- ANIS-Gamification/routes/web.php
+++ ANIS-Gamification/routes/web.php
@@ -11,3 +11,9 @@ Route::get('/', function () {
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register.show');
 
 Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth');

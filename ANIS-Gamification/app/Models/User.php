3d2b9cd399cb177eb44b30c9a2f4f8cc92693202
ajartijaouad <ajartijaouad14@gmail.com>
2026-04-17 12:22:39 +0100

    feat: install and configure Laravel Sanctum with API auth routes

--- ANIS-Gamification/app/Models/User.php
+++ ANIS-Gamification/app/Models/User.php
@@ -9,13 +9,14 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['pseudo', 'email', 'password', 'is_anonymous', 'xp_total', 'streak_days'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * Get the attributes that should be cast.

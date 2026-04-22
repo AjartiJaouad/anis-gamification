b2250b02bb21d336b3b3455d1de93a1d9e743ddf
ajartijaouad <ajartijaouad14@gmail.com>
2026-04-22 12:20:00 +0100

    feat: add store method for modules

--- ANIS-Gamification/app/Http/Controllers/Admin/ModuleController.php
+++ ANIS-Gamification/app/Http/Controllers/Admin/ModuleController.php
@@ -19,4 +19,22 @@ class ModuleController extends Controller
     {
         return view('admin.modules.create');
     }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+            'order' => 'nullable|integer|min:1',
+        ]);
+
+        Module::create([
+            'title' => $data['title'],
+            'content' => $data['content'],
+            'order' => $data['order'] ?? 1,
+        ]);
+
+        return redirect()->route('admin.modules.index')
+            ->with('success', 'Module cree avec succes.');
+    }
 }

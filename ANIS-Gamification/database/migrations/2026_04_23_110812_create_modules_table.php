d8ddde14fe6d474923f66d5e2dd36eb959870728
ajartijaouad <ajartijaouad14@gmail.com>
2026-04-23 12:10:29 +0100

    feat: add modules table structure

--- ANIS-Gamification/database/migrations/2026_04_23_110812_create_modules_table.php
+++ ANIS-Gamification/database/migrations/2026_04_23_110812_create_modules_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('modules', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->integer('order')->default(1);
             $table->timestamps();
         });
     }
